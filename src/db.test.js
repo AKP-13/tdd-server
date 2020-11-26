c72703d631858e8bba864b13caecf0072322cf58
AKP-13 <alexpeirson@hotmail.com>
2020-11-26 17:23:20 +0000

    Pass test of recieveing correct user from db given username

--- src/db.test.js
+++ src/db.test.js
@@ -5,17 +5,41 @@ import { getUserByUsername } from "./db";
 describe("getUserByUsername", () => {
     it("get the correct user from the database given a username", async () => {
         const client = await MongoClient.connect(
-            `mongodb://localhost:27017/TEST_DB`,
+            "mongodb://localhost:27017/TEST_DB",
             {
                 useNewUrlParser: true,
-                useUnifiedTopology: true
+                useUnifiedTopology: true,
+            }
+        );
+
+        const db = client.db("TEST_DB");
+
+        const fakeData = [
+            {
+                id: 123,
+                username: "abc",
+                email: "abc@gmail.com",
             },
-        ),
+            {
+                id: 124,
+                username: "wrong",
+                email: "wrong@gmail.com",
+            },
+        ];
 
-        const db = client.db('TEST_DB');
-
-        // tests
-
+        await db.collection("users").insertMany(fakeData);
+        const actual = await getUserByUsername("abc");
+        const finalDBState = await db.collection("users").find().toArray();
+        await db.dropDatabase();
         client.close();
+
+        const expected = {
+            id: 123,
+            username: "abc",
+            email: "abc@gmail.com",
+        };
+
+        expect(actual).excludingEvery("_id").to.deep.equal(expected);
+        expect(finalDBState).excludingEvery("_id").to.deep.equal(fakeData);
     });
 });
